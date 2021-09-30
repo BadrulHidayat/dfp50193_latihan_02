f577a8e1717db76b9896b4ea0265ece9dd9a607e
BadrulHidayat <badrulhidayat95@gmail.com>
2021-09-30 10:41:27 +0800

    latihan kedua

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ms">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan kedua</title>
+</head>
+<body>
+DDT5B 18DDT19F1005 BADRUL HIDAYAT BIN BAHAROM
+
+    <?php
+    $nama = 'Badrul';
+    $umur = 2021 - 2001;
+
+    echo "$nama berumur $umur tahun";
+    ?>
+</body>
+</html>
